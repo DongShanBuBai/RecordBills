6fed1a85618628493cb86ab5fb1957b8fde321ed
东山不败 <3621675360@qq.com>
2022-02-16 22:08:50 +0800

    first commit

---
+++ src/main/java/com/lil/maven/RecordBillsApplication.java
@@ -0,0 +1,16 @@
+package com.lil.maven;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class RecordBillsApplication
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(RecordBillsApplication.class,args);
+    }
+}
---
+++ src/test/java/com/lil/maven/AppTest.java
@@ -0,0 +1,20 @@
+package com.lil.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple RecordBillsApplication.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
