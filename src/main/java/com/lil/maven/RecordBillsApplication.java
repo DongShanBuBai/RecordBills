71b266e38f86ce5791304beebb8897015695804a
DongShanBuBai <3621675360@qq.com>
2022-02-22 20:52:26 +0800

    前后端连接测试

--- src/main/java/com/lil/maven/RecordBillsApplication.java
+++ src/main/java/com/lil/maven/RecordBillsApplication.java
@@ -1,5 +1,6 @@
 package com.lil.maven;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Hello world!
  *
  */
+@MapperScan("com.lil.maven.dao.mapper")     //扫描mapper接口
 @SpringBootApplication
 public class RecordBillsApplication
 {
