069f667eed041291652e60845a816517d2dec57e
DongShanBuBai <3621675360@qq.com>
2022-02-23 17:54:40 +0800

    前后端连接测试

--- src/main/java/com/lil/maven/dao/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lil.maven.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lil.maven.pojo.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserMapper extends BaseMapper<User>{
-}
--- src/main/java/com/lil/maven/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.lil.maven.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.lil.maven.pojo.User;
-
-public interface UserService extends IService<User> {
-}
