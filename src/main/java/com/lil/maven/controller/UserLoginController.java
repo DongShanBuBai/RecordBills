7fc0afb268982c4fcc523434f2fe4e433a64234a
DongShanBuBai <3621675360@qq.com>
2022-02-26 21:55:58 +0800

    添加响应体类型和响应码

--- src/main/java/com/lil/maven/controller/UserLoginController.java
+++ src/main/java/com/lil/maven/controller/UserLoginController.java
@@ -1,8 +1,22 @@
 package com.lil.maven.controller;
 
+import com.lil.maven.service.UserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @Author:lil
  * @Date: 2022-02-23
  */
+@RestController
+@RequestMapping("/login")
 public class UserLoginController {
+    @Autowired
+    UserLoginService userLoginService;
+    @PostMapping("/weChatLogin")
+    public void weChatLogin(String code){
+        System.out.println(code);
+        userLoginService.weChatUserLoginService(code);
+    }
 }
+
